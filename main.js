c8932e8cc43f7c937b001a85b11d068d5c6e8443
uiems0 <alexshorty568@gmail.com>
2026-08-20 19:13:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+let TopCharsPM = document.getElementsByClassName("BPM")
+let CurTopCharsPM = document.getElementsByClassName("CPM")
+let BestAcc = document.getElementsByClassName("BA")
+let CurrAcc = document.getElementsByClassName("CA")
+let Button = document.getElementsByClassName("StartButton")
+let TextArea = document.getElementsByClassName("TextArea")
 
+let CurrRunning = false
+let CurrTime
+let CurrString
+
+let PossibleStrings = new Array("This is some Random Text as I do not know what else to write for this. I haven't used Java Script in a while but thats fine.")
+
+Button:addEventListener("click", () => {
+  if (CurrRunning == false) {
+  CurrRunning = true
+  let NewString = PossibleStrings[Math.round(Math.random * PossibleStrings.length)] 
+  CurrString = NewString
+  } else {
+
+  }
+})
