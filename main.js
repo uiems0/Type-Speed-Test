2715c989b1f4ba72545b1971aa5910f72ca0b667
uiems0 <alexshorty568@gmail.com>
2026-08-21 19:56:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let BestAcc = document.getElementsByClassName("BA")
 let CurrAcc = document.getElementsByClassName("CA")
 let Button = document.getElementsByClassName("StartButton")
 let TextArea = document.getElementsByClassName("TextArea")
-
+let TypingArea = document.getElementsByClassName("TypingArea")
 let CurrRunning = false
 let CurrTime
 let CurrString
@@ -16,7 +16,20 @@ Button:addEventListener("click", () => {
   CurrRunning = true
   let NewString = PossibleStrings[Math.round(Math.random * PossibleStrings.length)] 
   CurrString = NewString
+  TextArea.textcontent = CurrString
+  CurrTime = Date.now()
   } else {
-
+    let TimeRes = Date.now() - CurrTime
+    let Length = CurrString.length
+    let TypedString = TypingArea.textcontent
+    let Correct = 0
+    for (let i = 0;i<TypedString;i++)  {
+      let CurStringNum = CurrString[i]
+      let CurTypedString = TypedString[i]
+      if (CurStringNum == CurTypedString) {
+        Correct += 1
+      }
+    }
+    //Add logic for extra/not enough chars to factor into chars per min and acc
   }
 })
