9796ad01b54646d6079c1c1c30210adbe6389c1f
uiems0 <alexshorty568@gmail.com>
2026-08-25 07:05:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,29 +5,31 @@ let CurrAcc = document.getElementsByClassName("CA")
 let Button = document.getElementsByClassName("StartButton")
 let TextArea = document.getElementsByClassName("TextArea")
 let TypingArea = document.getElementsByClassName("TypingArea")
+
 let CurrRunning = false
 
 let CurrTime
-let CurrString
+let CurrString = ""
 
 let TopCharsPerMin = 0
 let BestAcc = 0
 
 let PossibleStrings = new Array("This is some Random Text as I do not know what else to write for this. I haven't used Java Script in a while but thats fine.")
 
-Button:addEventListener("click", () => {
+Button.addEventListener("click", () => {
   if (CurrRunning == false) {
   CurrRunning = true
-  let NewString = PossibleStrings[Math.round(Math.random * PossibleStrings.length)] 
+  let NewString = PossibleStrings[Math.floor(Math.round() * NewString.length)] 
+  console.log(NewString)
   CurrString = NewString
-  TextArea.textcontent = CurrString
+  TextArea.textContent = CurrString
   CurrTime = Date.now()
   } else {
     let TimeRes = Date.now() - CurrTime
     let Length = CurrString.length
-    let TypedString = TypingArea.textcontent
+    let TypedString = TypingArea.textContent
     let Correct = 0
-    for (let i = 0;i<TypedString;i++)  {
+    for (let i = 0;i<TypedString.length;i++)  {
       let CurStringNum = CurrString[i]
       if (CurStringNum == null) {
         Correct -=1
@@ -39,15 +41,15 @@ Button:addEventListener("click", () => {
       }
     }
     let AccPercent = (Correct/Length) * 100
-    let CPM = AccPercent * (TimeRes / 1000)
-    CurCharsPM.textcontent = CPM
-    CurrAcc.textcontent = AccPercent
+    let CPM = AccPercent * (TimeRes / 60000)
+    CurCharsPM.textContent = CPM
+    CurrAcc.textContent = AccPercent
     if (CPM > TopCharsPerMin) {
-      TopCharsPM.textcontent = CPM
+      TopCharsPM.textContent = CPM
       TopCharsPerMin = CPM
     }
     if (AccPercent > BestAcc) {
-      BestAccEl.textcontent = AccPercent
+      BestAccEl.textContent = AccPercent
       BestAcc = AccPercent
     }
   }
